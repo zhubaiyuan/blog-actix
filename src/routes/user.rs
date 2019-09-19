73c30acdf1a76f86071569c6041629989f9c2963
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 18:47:53 +0800

    define a struct to represent the input to create a user

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -3,3 +3,8 @@ use crate::routes::convert;
 use crate::{models, Pool};
 use actix_web::{web, HttpResponse};
 use futures::Future;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserInput {
+    username: String,
+}
