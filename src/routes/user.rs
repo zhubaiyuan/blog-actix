968e62389f256740d919a70d5cdf402d9e7c68c9
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 18:51:43 +0800

    handler create_user

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -8,3 +8,15 @@ use futures::Future;
 struct UserInput {
     username: String,
 }
+
+fn create_user(
+    item: web::Json<UserInput>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError> {
+    web::block(move || {
+        let conn: &SqliteConnection = &pool.get().unwrap();
+        let username = item.into_inner().username;
+        models::create_user(conn, username.as_str())
+    })
+    .then(convert)
+}
