dba0137450295eb85083b6c49e1d59cf55ee71fe
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 17:27:55 +0800

    create a type alias for our pool of database connections

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,5 @@ extern crate serde_derive;
 use actix_web::{middleware, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
+
+type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
