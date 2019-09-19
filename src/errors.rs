ef256a7509a817a78d3177794922a7f4dc60258f
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 17:45:29 +0800

    define the type AppError

--- src/errors.rs
+++ src/errors.rs
@@ -3,3 +3,11 @@ use actix_web::web::HttpResponse;
 use diesel::result::DatabaseErrorKind::UniqueViolation;
 use diesel::result::Error::{DatabaseError, NotFound};
 use std::fmt;
+
+#[derive(Debug)]
+pub enum AppError {
+    RecordAlreadyExists,
+    RecordNotFound,
+    DatabaseError(diesel::result::Error),
+    OperationCanceled,
+}
