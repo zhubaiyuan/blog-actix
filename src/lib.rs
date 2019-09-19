e7b229eff1b1b307bd915c6674c37b796c1cee5b
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 17:37:23 +0800

    add functionality new and run to struct Blog

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,26 @@ type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 pub struct Blog {
     port: u16,
 }
+
+impl Blog {
+    pub fn new(port: u16) -> Self {
+        Blog { port }
+    }
+
+    pub fn run(&self, database_url: String) -> std::io::Result<()> {
+        let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+        let pool = r2d2::Pool::builder()
+            .build(manager)
+            .expect("Failed to create pool.");
+
+        println!("Starting http server: 127.0.0.1:{}", self.port);
+        HttpServer::new(move || {
+            App::new()
+                .data(pool.clone())
+                .wrap(middleware::Logger::default())
+                .configure(routes::users::configure)
+        })
+        .bind(("127.0.0.1", self.port))?
+        .run()
+    }
+}
