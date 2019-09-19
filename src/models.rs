1d96b4562e9ce2b9e2242dfd2c810886591a49e4
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 18:22:29 +0800

    define a struct User to repreent a user in the database

--- src/models.rs
+++ src/models.rs
@@ -3,3 +3,9 @@ use crate::schema::users;
 use diesel::prelude::*;
 
 type Result<T> = std::result::Result<T, AppError>;
+
+#[derive(Queryable, Identifiable, Serialize, Debug, PartialEq)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+}
