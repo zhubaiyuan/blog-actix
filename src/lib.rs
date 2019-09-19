330b532f318ba0822014005fdc512ffb351b0723
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 17:24:28 +0800

    import/ set up the library

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+
+use actix_web::{middleware, App, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
