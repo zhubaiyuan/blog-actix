7eeed9fd0bf80639a1238182c4159203ca7efcb2
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-19 17:32:39 +0800

    specify the modules that make up our crate in lib

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,9 @@ use actix_web::{middleware, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
+mod errors;
+mod models;
+mod routes;
+mod schema;
+
 type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
