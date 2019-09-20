b5bc2434c69ba0f320472e5bb6d872459bc0286b
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-20 08:23:07 +0800

    handler function add_post

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -10,3 +10,21 @@ struct PostInput {
     title: String,
     body: String,
 }
+
+fn add_post(
+    user_id: web::Path<i32>,
+    post: web::Json<PostInput>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError> {
+    web::block(move || {
+        let conn: &SqliteConnection = &pool.get().unwrap();
+        let key = models::UserKey::ID(user_id.into_inner());
+        models::find_user(conn, key).and_then(|user| {
+            let post = post.into_inner();
+            let title = post.title;
+            let body = post.body;
+            models::create_post(conn, &user, title.as_str(), body.as_str())
+        })
+    })
+    .then(convert)
+}
