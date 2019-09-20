9ba4dda193b95023aab621c99bbdf4b8be4b63d1
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-20 09:04:21 +0800

    get comments on a post

--- src/models.rs
+++ src/models.rs
@@ -149,3 +149,12 @@ pub fn user_posts(
 
     Ok(posts.into_iter().zip(comments).collect())
 }
+
+pub fn post_comments(conn: &SqliteConnection, post_id: i32) -> Result<Vec<(Comment, User)>> {
+    comments::table
+        .filter(comments::post_id.eq(post_id))
+        .inner_join(users::table)
+        .select((comments::all_columns, (users::id, users::username)))
+        .load::<(Comment, User)>(conn)
+        .map_err(Into::into)
+}
