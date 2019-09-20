de7c603b4a14e07aa03ea6cd2999b1593f7548b3
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-09-20 08:38:00 +0800

    create and crop the table comments

--- migrations/2019-09-20-003400_create_comments/up.sql
+++ migrations/2019-09-20-003400_create_comments/up.sql
@@ -1,1 +1,6 @@
--- Your SQL goes here
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users (id),
+    post_id INTEGER NOT NULL REFERENCES posts (id),
+    body TEXT NOT NULL
+)
